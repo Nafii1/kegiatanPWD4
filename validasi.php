83cc00f46b76f33e233374e6fa47d2aa600bfd96
Nafii1 <92018120+Nafii1@users.noreply.github.com>
2021-10-27 21:36:26 +0700

    Kegiatan PT 4 - PWD - 1900018287

---
+++ validasi.php
@@ -0,0 +1,138 @@
+<html>
+
+ <head>
+ <style>
+ .error {color: #FF0000;}
+ </style>
+ </head>
+
+ <body>
+<center>
+ <?php
+ // define variables and set to empty values
+ $namaErr = $emailErr = $genderErr = $websiteErr = "";
+ $nama = $email = $gender = $comment = $website = "";
+
+ if ($_SERVER["REQUEST_METHOD"] == "POST") {
+ if (empty($_POST["nama"])) {
+ $namaErr = "Nama harus diisi";
+ }else {
+ $nama = test_input($_POST["nama"]);
+ }
+
+ if (empty($_POST["email"])) {
+ $emailErr = "Email harus diisi";
+ }else {
+ $email = test_input($_POST["email"]);
+
+ // check if e-mail address is well-formed
+ if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+ $emailErr = "Email tidak sesuai format";
+ }
+ }
+ if (empty($_POST["website"])) {
+ $website = "";
+ }else {
+ $website = test_input($_POST["website"]);
+ }
+
+ if (empty($_POST["comment"])) {
+ $comment = "";
+ }else {
+ $comment = test_input($_POST["comment"]);
+ }
+
+ if (empty($_POST["gender"])) {
+ $genderErr = "Gender harus dipilih";
+ }else {
+ $gender = test_input($_POST["gender"]);
+ }
+ }
+
+ function test_input($data) {
+ $data = trim($data);
+ $data = stripslashes($data);
+ $data = htmlspecialchars($data);
+ return $data;
+ }
+ ?>
+ <h2>Posting Komentar </h2>
+
+ <p><span class = "error">* Harus Diisi.</span></p>
+
+ <form method = "post" action = "<?php
+ echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+ <table>
+ <tr>
+ <td>Nama:</td>
+ <td><input type = "text" name = "nama">
+ <span class = "error">* <?php echo $namaErr;?></span>
+ </td>
+ </tr>
+
+ <tr>
+ <td>E-mail: </td>
+ <td><input type = "text" name = "email">
+ <span class = "error">* <?php echo $emailErr;?></span>
+ </td>
+ </tr>
+
+ <tr>
+ <td>Website:</td>
+ <td> <input type = "text" name = "website">
+ <span class = "error"><?php echo $websiteErr;?></span>
+ </td>
+ </tr>
+
+ <tr>
+ <td>Komentar:</td>
+ <td> <textarea name = "comment" rows = "5" cols = "40"></textarea></td>
+ </tr>
+
+ <tr>
+ <td>Gender:</td>
+ <td>
+ <input type = "radio" name = "gender" value = "L">Laki-Laki
+ <input type = "radio" name = "gender" value = "P">Perempuan
+ <span class = "error">* <?php echo $genderErr;?></span>
+ </td>
+ </tr>
+ <td>
+ <input type = "submit" name = "submit" value = "Submit">
+ </td>
+ </table>
+ </form>
+
+ <?php
+ echo "<br><br<h3>BERIKUT DATA YANG TELAH DI ISI :</h3><br>";
+ echo "<br></br>";
+ 
+ echo "<table width='450' border='5' cellpadding='15' cellspacing='1';>";
+
+ echo "<tr>";
+ echo "<th>Nama</th>";
+ echo "<th> ", $nama, "</th>";
+ echo "</tr>";
+ echo "<tr>";
+ echo "<th>Email</th>";
+ echo "<th> ", $email, "</th>";
+ echo "</tr>";
+ echo "<tr>";
+ echo "<th>Website</th>";
+ echo "<th> ", $website, "</th>";
+ echo "</tr>";
+ echo "<tr>";
+ echo "<th>Komentar</th>";
+ echo "<th> ", $comment, "</th>";
+ echo "</tr>";
+ echo "<tr>";
+ echo "<th>Gender</th>";
+ echo "<th> ", $gender, "</th>";
+ echo "</tr>";
+
+ echo "</table>";
+ ?>
+
+ </body>
+</html>
+</center>
